cffa87ffabf39cc5d28f6558e163aa1116bade7f
Rohit Kukreja <rohit.kukreja01@gmail.com>
2021-06-08 00:28:58 +0530

    Create animation.py

---
+++ animation.py
@@ -0,0 +1,26 @@
+images = [] # Put images as numpy arrays in this list
+
+import numpy as np
+from IPython.display import HTML
+import matplotlib
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+matplotlib.rcParams['animation.embed_limit'] = 2**64
+fig = plt.figure(figsize=(8,8))
+plt.axis("off")
+ims = []
+
+
+from matplotlib import animation
+for j,i in tqdm(enumerate(images)):
+    ims.append([plt.imshow(np.transpose(i,(1,2,0)), animated=True)]) 
+    
+
+ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+HTML(ani.to_jshtml())
+f = "animation{}.gif".format(datetime.datetime.now()).replace(":","")
+
+from matplotlib.animation import PillowWriter
+
+ani.save(os.path.join(wandb.run.dir,f), writer=PillowWriter(fps=20)) 
+ani.save(f, writer=PillowWriter(fps=20)) 
